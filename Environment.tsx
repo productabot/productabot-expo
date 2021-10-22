72699dddadc1c19e1071d835635efb0affc79654
Chris Aitken <chris@heythisischris.com>
2021-10-21 20:50:25 -0400

    Exposing environment config with secrets to allow EAS build.

---
+++ Environment.tsx
@@ -0,0 +1,8 @@
+export const Environment = {
+    endpoint: process.env.endpoint,
+    region: process.env.region,
+    identityPoolId: process.env.identityPoolId,
+    userPoolId: process.env.userPoolId,
+    userPoolWebClientId: process.env.userPoolWebClientId,
+    bucket: process.env.bucket,
+}
