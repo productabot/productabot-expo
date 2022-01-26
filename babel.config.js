f5d8034a59020b7287528626232ebe5f37f15555
Chris Aitken <chris@heythisischris.com>
2022-01-26 01:45:10 -0500

    Updated to Expo SDK 44. Updated environment to support EAS Build and Submit.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { lazyImports: true }]]
+    presets: [['babel-preset-expo', { lazyImports: true }]],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
